d23b87303e81eddbc80aa3575aebfd5d3db7ca5d
Дутова Ольга Алексеевна <OUT-Dutova2-OA@sigma.sbrf.ru>
2020-11-25 18:12:05 +0300

    Api1

---
+++ MyApi/Api1/Api1/Models/FileLoggerProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Api1.Models
+{
+    public class FileLoggerProvider: ILoggerProvider
+    {
+        private string path;
+        public FileLoggerProvider(string _path)
+        {
+            path = _path;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
