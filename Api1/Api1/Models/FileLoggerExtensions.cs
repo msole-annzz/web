c7153bb0bc1a676621ede428fbce46206494a776
Дутова Ольга Алексеевна <OUT-Dutova2-OA@sigma.sbrf.ru>
2020-11-28 16:34:27 +0300

    delete weather forecast

---
+++ Api1/Api1/Models/FileLoggerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+
+namespace Api1.Models
+{
+    public static class FileLoggerExtensions
+    {
+        public static ILoggerFactory AddFile(this ILoggerFactory factory,
+                                        string filePath)
+        {
+            factory.AddProvider(new FileLoggerProvider(filePath));
+            return factory;
+        }
+    }
+}
---
+++ Api1/Api1/Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Api1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+
+namespace Api1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Добавляет контекст базы данных в контейнер внедрения зависимостей.
+           // Указывает, что контекст базы данных будет использовать базу данных в памяти.
+            services.AddDbContext<TodoContext>(opt =>
+              opt.UseInMemoryDatabase("TodoList"));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //для логирования
+           // loggerFactory.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logger.txt"));
+            loggerFactory.AddFile("logger.txt");
+            var logger = loggerFactory.CreateLogger("FileLogger");
+            // HttpContext context1 = null;
+           // app.Run(async (context) =>
+           //{
+               //context1 = context;
+               //await context.Response.WriteAsync("Welcome to ToDoIst");
+              // logger.LogInformation("Processing request {0}", context.Request.Path);
+          // });
+
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                
+             //   logger.LogInformation("Processing request {0}", context1.Request.Path);
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
