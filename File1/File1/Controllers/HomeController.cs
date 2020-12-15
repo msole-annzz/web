bb3de251c4fafe5463a9043c04627ae04c74004b
msole-annzz <57389666+msole-annzz@users.noreply.github.com>
2020-12-15 09:52:39 +0300

    File1
    File1

---
+++ File1/File1/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using File1.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileUploadApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationContext _context;
+        IWebHostEnvironment _appEnvironment;
+
+        public HomeController(ApplicationContext context, IWebHostEnvironment appEnvironment)
+        {
+            _context = context;
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            _context.Files.Load();
+            return View(_context.Files.ToList());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))//using чистит выделенную память, выделенную
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                _context.Files.Add(file);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> AddFile(IFormFileCollection uploads)
+        //{
+        //    foreach (var uploadedFile in uploads)
+         //   {
+                // путь к папке Files
+           //     string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+           //     using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+            //    {
+            //        await uploadedFile.CopyToAsync(fileStream);
+            //    }
+           //     FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+           //     _context.Files.Add(file);
+           // }
+           // _context.SaveChanges();
+
+        //    return RedirectToAction("Index");
+       // }
+    }
+}
---
+++ File1/File1/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace File1.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<FileModel> Files { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
